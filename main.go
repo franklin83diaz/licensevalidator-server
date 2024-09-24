a23cf287d57423e7278dd97e14c70f3a2eca7b37
Franklin Diaz <fdiaz@adaptivecomputing.com>
2024-09-24 16:41:52 -0400

    config file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -20,8 +21,21 @@ func main() {
 	config.AddDriver(toml.Driver)
 
 	// Load config from files
-	err := config.LoadFiles(configFile)
+	err := config.LoadFilesByFormat("toml", configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	//Read
+	httpsCertPath := config.String("https.cert_path")
+	httpsKeyPath := config.String("https.key_path")
+	fmt.Println("Ruta del certificado HTTPS:", httpsCertPath)
+	fmt.Println("Ruta de la clave privada HTTPS:", httpsKeyPath)
+
+	jwtPrivateKeyPath := config.String("jwt.private_key_path")
+	jwtPublicKeyPath := config.String("jwt.public_key_path")
+	jwtAlgorithm := config.String("jwt.algorithm")
+	fmt.Println("Ruta de la clave privada JWT:", jwtPrivateKeyPath)
+	fmt.Println("Ruta de la clave pública JWT:", jwtPublicKeyPath)
+	fmt.Println("Algoritmo de firma JWT:", jwtAlgorithm)
 }
